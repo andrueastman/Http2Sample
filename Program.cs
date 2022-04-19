23270d82409fd37b2b9267582f475881983c4c72
Andrew Omondi <andrew.omondi@microsoft.com>
2022-04-19 18:37:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+
+namespace GraphV4Sample
+{
+    class Program
+    {
+        public static async Task Main(string[] _)
+        {
+            string[] scopes = new[] { "User.Read", "User.ReadWrite" };
+            InteractiveBrowserCredentialOptions interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = "CLIENT_ID"
+            };
+            var interactiveBrowserCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+            var authProvider = new TokenCredentialAuthProvider(interactiveBrowserCredential,scopes);
+
+            var graphClient = new GraphServiceClient("https://microsoftgraphvalidation.ags.msidentity.com/v1.0", authProvider);
+
+            var me = await graphClient.Me.Request().GetAsync();
+        }
+    }
+}
